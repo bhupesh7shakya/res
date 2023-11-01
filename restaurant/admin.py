2418d24ae9e28d730c1432769f4c793f0169009a
bhupesh shakya <bhupesh7shakya@gmail.com>
2023-11-01 16:25:34 +0545

    admin model

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=('name',)
+    list_per_page=10
+    search_fields=('name',)
--- restaurant/models.py
+++ restaurant/models.py
@@ -5,6 +5,9 @@ User=get_user_model()
 
 class Category(models.Model):
     name=models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+        return self.name
 
 class Food(models.Model):
     name=models.CharField(max_length=255)
