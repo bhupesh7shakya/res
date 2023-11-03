0596357cc7712fbb265b22755446015f560538d7
bhupesh shakya <bhupesh7shakya@gmail.com>
2023-11-03 16:14:44 +0545

    category serializer get api

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import CategorySerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
+@api_view(['GET'])
+def category_list(request):
+    categories = Category.objects.all()
+    serializer=CategorySerializer(categories,many=True)
+    return Response(serializer.data,status=status.HTTP_200_OK)
